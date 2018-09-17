40e5a1f9c41f31c821adc1f108470f3e96ee0fea
StaphiX <superdan_1990@hotmail.co.uk>
2018-09-17 20:11:30 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = function(env, argv) {
+
+// default to the server configuration
+const base = {
+        entry: "./src/server.js",
+        output: {
+            filename: "server.js",
+            path: __dirname + "/dist"
+        },
+
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: "source-map",
+
+        resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: [".ts", ".tsx", ".js", ".json"]
+        },
+
+        module: {
+            rules: [
+                // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+                { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                { enforce: "pre", test: /\.js$/, loader: "source-map-loader", exclude: '/node_modules/'}
+            ]
+        },
+
+        // When importing a module whose path matches one of the following, just
+        // assume a corresponding global variable exists and use that instead.
+        // This is important because it allows us to avoid bundling all of our
+        // dependencies, which allows browsers to cache those libraries between builds.
+        externals: [
+            {
+                "react": "React",
+                "react-dom": "ReactDOM",
+            },
+                nodeExternals()
+            ]       
+    }
+
+  // server-specific configuration
+  if (env.platform === 'server') {
+    base.target = 'node';
+  }
+
+  // client-specific configurations
+  if (env.platform === 'client') {
+    base.target = 'react';
+    base.entry = './src/index.tsx';
+    base.output.filename = 'js/client.js';
+  }
+
+  return base;
+};
